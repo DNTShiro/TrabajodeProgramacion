a06895ed51e4237d8bec693298e5ea64ff9a4bf8
DNTShiro <107779105+DNTShiro@users.noreply.github.com>
2026-01-18 22:44:03 -0500

    Agrega clase Cliente con encapsulamiento

---
+++ Cliente.java
@@ -0,0 +1,22 @@
+public class Cliente {
+    private String dni;
+    private String nombre;
+
+    public Cliente(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String toString() {
+        return "Cliente: " + nombre + " | DNI: " + dni;
+    }
+}
+
